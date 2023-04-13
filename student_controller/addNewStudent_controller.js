ecc5199d8c86ac1671d1f8e11d8462a040187495
AMR-SIAM <65768688+AMR-SIAM@users.noreply.github.com>
2023-04-13 17:23:04 +0200

    edit and delete using mongodb

--- student_controller/addNewStudent_controller.js
+++ student_controller/addNewStudent_controller.js
@@ -40,44 +40,46 @@ const addStudent = (req, res) => {
     res.send(student);
 };
 
-const editStudent = (req, res) => {
-    //we check if the id exist or not
-    // Parsent transforms string id to int
-    const student = computer_dep_students.find(c => c.id === parseInt(req.params.id));
+const editStudent = async (req, res) => {
     
-  /*  if (!student)// in case there is no match in id
-
-        res.status(404).send('The student with the given ID not found !');
-
-    const result = validateStudentId(req.body); /// validate data from .helper file with  joi
-
-    if (result.error) {
-        // details[0] prints the firs line of the error
-        res.status(400).send(result.error.details[0].message);
-        console.log(result);
-        return;*/
-
+    try {
+        
+        const student = await computer_dep_students.findOne({ _id: req.params.id });
+        if (req.body.student_name) {
+            student.student_name = req.body.student_name;
+        }
+        if (req.body.student_dep) {
+            student.student_dep = req.body.student_dep;
+        }
+        if (req.body.student_UniversityID) {
+            student.student_UniversityID = req.body.student_UniversityID;
+        }
+        if (req.body.academic_year) {
+            student.academic_year = req.body.academic_year;
+        }
+        if (req.body.degree) {
+            student.degree = req.body.degree;
+        }
+        if (req.body.subjects) {
+            student.subjects = req.body.subjects;
+        }
+        await student.save();
+        res.status(200).send(student);
+    } catch (error) {
+        res.status(400).send(error);
     }
-    // we get the new name from api body
-
-    student.student_name = req.body.student_name;
-    //we send the new data to apii as a response
-    res.send(student);
-
 };
 
-const deleteStudent = (req, res) => {
-    // serach for the id we want to delet if it is exist or not
-    const student = computer_dep_students.find(c => c.id === parseInt(req.params.id));
-    if (!student)
-        res.status(404).send('The Course with the given ID not found !');
-    /// we get the index of that object
-    const index = computer_dep_students.indexOf(student);
-    /// delet object
-    computer_dep_students.splice(index, 1);
-
-    res.send(student);
+const deleteStudent = async (req, res) => {
+    try {
+        const student = await computer_dep_students.deleteOne({ _id: req.params.id });
+        res.status(200).send(student);
+    } catch (error) {
+        res.status(400).send(error);
+        
+    }
 };
+
 
 module.exports = {
     getAllstudents,
