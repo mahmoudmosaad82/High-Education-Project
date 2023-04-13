22c7f87ca2a76e70ee209d3315284123425b6af6
AMR-SIAM <65768688+AMR-SIAM@users.noreply.github.com>
2023-04-13 17:17:32 +0200

    make validation using mongoose Schema

--- models/department_students.js
+++ models/department_students.js
@@ -1,8 +1,58 @@
-const computer_dep_students = [
+
+
+
+
+
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+
+const StudentSchema= Schema({
+    student_name: {
+        type: String,
+        required: true,
+    },
+    student_dep: {
+        type: String,
+        required: true,
+    },
+    student_UniversityID: {
+        type: Number,
+        required: true,
+    },
+ 
+  
+    academic_year: {
+        type: Number,
+        required: true,
+    },
+
+    degree: {
+        type: String,
+        required: true,
+    },
+    subjects: [{type:String ,
+        required: true,},{type:String ,
+            required: true,},{type:String ,
+                required: true,},
+        
+    ]
+       
+    
+});
+
+const computer_dep_students = model('computer_dep_students', StudentSchema);
+
+
+
+
+/*const computer_dep_students = [
     {id:1 , student_name:"Mahmoud",student_dep:"computer",
     student_UniversityID:1,academic_year:2021,degree:"bachelor",subjects:["Ai","ML","NN"]},
    
-];
+];*/
 
 /* 
 const electrical_dep_students = [
