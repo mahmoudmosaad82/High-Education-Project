f8774c43a54fac03b206d9a5f0e06497dd57f84b
Mahmoud Mousa <66203316+mahmoudmosaad82@users.noreply.github.com>
2023-04-13 17:30:49 +0200

    add MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const {app} = require('./app.js');
+const mongoose = require("mongoose");
 
-const port = process.env.PORT || 3200;
 
-app.listen(port,()=>  console.log(`listening on port ${port} ...`));
+const port = process.env.PORT || 3000;
+
+app.listen(port, async () => {
+    await mongoose.connect('mongodb+srv://amrsiam:a3332862@cluster0.9djjbot.mongodb.net/?retryWrites=true&w=majority');
+    console.log(`listening on port ${port} ...`)
+});
